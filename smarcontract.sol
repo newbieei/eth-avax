8d02fd91cbd9dded123a7d4f1835bc040bdb731a
newbieei <98467891+newbieei@users.noreply.github.com>
2023-06-30 18:05:10 +0800

    Create smarcontract.sol

---
+++ smarcontract.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+
+contract SmartContract {
+  
+    uint public balance;
+    
+    
+    function requireBalance (uint _balance) external {
+        require(_balance > 0,"Shoud maintain 1 peso");
+        balance = _balance;
+    }
+
+    function freeBalance() external {
+        if (balance >= 1)
+        assert (balance + 500 >= balance);
+        balance = balance + 500;
+    }
+
+    function numberRevert (uint _balance) external {
+        if (balance == _balance){
+                revert("Should obtain new balance");
+        }
+        balance = _balance;
+    }
+}
